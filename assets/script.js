1867e92a6ac7e9891ac5a2af5fbbe383c57d15ee
Anthony <cicconeworldwide@gmail.com>
2023-07-25 01:26:11 -0600

    initial commit

---
+++ assets/script.js
@@ -0,0 +1,186 @@
+var startButton = document.getElementById("startButton");
+startButton.addEventListener("click", beginQuiz);
+var answerButton = document.getElementsByClassName("answerChoice");
+var correctAnswer = 0;
+var wrongAnswer = 0;
+var viewHighScoresLink = document.getElementById("viewHighScores");
+viewHighScoresLink.addEventListener("click", playAgain);
+    
+for (var i = 0; i < answerButton.length; i++) {
+  answerButton[i].addEventListener("click", checkAnswer);
+}
+
+document.getElementById("answerOptions");
+answerOptions.style.display = "none";
+
+var timeEl = document.getElementById("countdown");
+var secondsLeft = 60;
+var currentQuestionIndex = 0;
+
+var questions = [
+  {
+    question: "What is the capital of Idaho?",
+    options: ["Venice", "Dallas", "Dublin", "Boise"],
+    correctIndex: 3,
+    correctAnswer: "Boise",
+  },
+
+  {
+    question: "What does CSS stand for?",
+    options: [
+      "Cows slay seals",
+      "Computers showing syle ",
+      "Cascading style sheet",
+      "Corrpution surf style",
+    ],
+    correctIndex: 2,
+    correctAnswer: "Cascading style sheet",
+  },
+
+  {
+    question: "Should you get back with your ex?",
+    options: [
+      ">>>",
+      ">>>",
+      "No",
+      "<<<",
+    ],
+    correctIndex: 2,
+    correctAnswer: "No",
+  },
+];
+
+function showAnswerButtons() {
+  var answerOptions = document.getElementById("answerOptions");
+  answerOptions.style.display = "block";
+}
+
+function setTime() {
+  var timeInterval = setInterval(function () {
+    secondsLeft--;
+    timeEl.textContent = "Time: " + secondsLeft;
+
+    if (secondsLeft <= 0) {
+      clearInterval(timeInterval);
+      timeEl.textContent = "0";
+      displayScore();
+    }
+  }, 1000);
+}
+
+function displayQuestion() {
+  var questionText = document.getElementById("questionText");
+  var answerOptions = document.getElementById("answerOptions");
+  
+  var currentQuestion = questions[currentQuestionIndex];
+  questionText.textContent = currentQuestion.question;
+  
+  var answerChoice = answerOptions.getElementsByClassName("answerChoice");
+  for (var i = 0; i < currentQuestion.options.length; i++) {
+    answerChoice[i].textContent = currentQuestion.options[i];
+  }
+}
+
+function beginQuiz() {
+  setTime();
+  displayQuestion();
+  showAnswerButtons();
+}
+
+function checkAnswer(event) {
+  var selectedAnswer = event.target.textContent;
+  var currentQuestion = questions[currentQuestionIndex];
+
+  if (
+    selectedAnswer === currentQuestion.options[currentQuestion.correctIndex]
+  ) {
+    correctAnswer++;
+  } else {
+    wrongAnswer++;
+    secondsLeft -= 5;
+  }
+
+  currentQuestionIndex++;
+
+  if (currentQuestionIndex < questions.length) {
+    displayQuestion();
+  } else {
+    displayScore();
+  }
+}
+
+function displayScore() {
+  var scoreEl = document.getElementById("score");
+  scoreEl.textContent =
+    "Answered correctly: " +
+    correctAnswer +
+    " Answered incorrectly: " +
+    wrongAnswer;
+
+  var nameInput = document.createElement("input");
+  nameInput.setAttribute("type", "text");
+  nameInput.setAttribute("placeholder", "Enter your initials");
+
+  var submitButton = document.createElement("button");
+  submitButton.textContent = "Submit";
+  submitButton.addEventListener("click", playAgain);
+  
+  scoreEl.appendChild(nameInput);
+  scoreEl.appendChild(submitButton);
+  nameInput.style.display = "flex";
+  nameInput.style.display = "column";
+
+  submitButton.addEventListener("click", function () {
+    var initials = nameInput.value.trim();
+    if (initials) {
+      localStorage.setItem("userInitials", initials);
+    }
+  });
+}
+
+
+function playAgain() {
+  currentQuestionIndex = 0;
+  correctAnswer = 0; 
+  wrongAnswer = 0; 
+  secondsLeft = 60;
+
+  var scoreEl = document.getElementById("score");
+  scoreEl.textContent = "Score submitted";
+
+  var nameInput = document.createElement("input");
+  nameInput.setAttribute("type", "text"); 
+  nameInput.setAttribute("placeholder", "Enter your initials"); 
+
+  var savedInitials = localStorage.getItem("userInitials");
+  if (savedInitials) {
+    var initialsDisplay = document.createElement("p");
+    initialsDisplay.textContent = "Your initials: " + savedInitials;
+    scoreEl.appendChild(initialsDisplay);
+  }
+
+  var resetButton = document.createElement("button");
+  resetButton.textContent = "Clear scores";
+  resetButton.addEventListener("click", resetQuiz);
+  resetButton.style.display = "flex";
+  resetButton.style.display = "column";
+
+  var backButton = document.createElement("button");
+  backButton.textContent = "Play again";
+  backButton.addEventListener("click", startOver);
+  backButton.style.display = "flex";
+  backButton.style.display = "column";
+
+  scoreEl.appendChild(resetButton);
+  resetButton.style.display = "block";
+  scoreEl.appendChild(backButton);
+  
+  function resetQuiz() {
+    localStorage.removeItem("userInitials");
+    window.location.reload(); 
+  }
+  
+  function startOver() {
+    window.location.reload(); 
+  }
+}
---
+++ readme.md
@@ -0,0 +1,21 @@
+### User Story
+AS A coding boot camp student
+<br>
+I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+<br>
+SO THAT I can gauge my progress compared to my peers
+<hr>
+
+### Description
+
+This challenge was to create a code quiz, and a challenge indeed it was. Several criteria were required to complete this assignment. This included making time intervals, event listeners and several variables and functions. The amount of variables and functions I used for this challenge was far more than I have used thus far by a significant margin. Below is a screenshot of the starting screen before the game begins
+
+![Alt text](<Screenshot 2023-07-25 at 00.58.33.png>)
+
+<hr>
+
+### Reflection
+
+This challenge has be the hardest for me thus far. I may start adding reflections during my time in this bootcamp (depending on the assignment). Thankfully, I have been told my README files are meaningful enough so far, but I believe it is important for me to do this since I will be the one looking back at this after the bootcamp is finished. This will be a beneficial way to track my own improvement as well as my most obvious (and subtle) strengths and weaknesses. Syntax on JavaScript and jQueries has proven to be diffucult for me to grasp right away. Just like anything else though, it's practice, practice, practice. Now more than ever I understand why Google is everyone's best friend. 
+<hr>
+User story provided by University of Denver FSF coding bootcamp
